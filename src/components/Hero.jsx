dd8a106b169ef2d1e3405d6333d4ab55be93e1ca
Oussama Rida <102989849+oussamarida@users.noreply.github.com>
2023-12-07 09:50:35 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         </div>
         <div >
            <h1 className='font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2'>Hi , I m <span className="text-[#C1C5D9]">Oussama</span></h1>
-           <p className="mt-2 text-white-100 text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]">Hello I m oussama 23y Student IIR5 </p>
+           <p className="mt-2 text-white-100 text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]">Hello I m oussama 23y Software Engineer </p>
            </div>
            
       </div>
